b076cf990f929d7310dc84828ffe860ba466291a
Robson <robsondrumbass@gmail.com>
2023-02-22 10:01:13 -0300

    posinstall adminlte

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/teste', function () {
+    echo "teste";
+});
+
+Route::get('/teste2',function(){ echo 'teste2';});
+Route::get('/teste3',function(){ echo 'teste3';});
